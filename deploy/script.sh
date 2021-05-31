7209875e128414c69009ceeddfa4a0ad5ff294a2
tom <tomas.miklusicak.cn@fpt.sk>
2021-05-31 23:12:29 +0200

    Add =

--- deploy/script.sh
+++ deploy/script.sh
@@ -16,16 +16,16 @@ version: 0.0
 Resources:
   - myLambdaFunction:
       Type: AWS::Lambda::Function
-	  Properties:
-	    Name: "$DEPLOY_FUNCTION_NAME"
-		Alias: "$DEPLOY_ALIAS_NAME"
-		CurrentVersion: "$DEVELOPMENT_ALIAS_VERSION"
-		TargetVersion: "$LATEST_VERSION"
+      Properties:
+        Name: "$DEPLOY_FUNCTION_NAME"
+	Alias: "$DEPLOY_ALIAS_NAME"
+	CurrentVersion: "$DEVELOPMENT_ALIAS_VERSION"
+	TargetVersion: "$LATEST_VERSION"
 EOM
 
 aws s3 cp $DEPLOY_APPSPEC_FILE s3://$DEPLOY_BUCKET_NAME/$DEPLOY_APPSPEC_FILE
 
-export REVISION="revisionType=S3,s3Location{bucket=$DEPLOY_BUCKET_NAME,key=$DEPLOY_BUCKET_NAME/$DEPLOY_APPSPEC_FILE,bundleType=YAML}"
+export REVISION="revisionType=S3,s3Location={bucket=$DEPLOY_BUCKET_NAME,key=$DEPLOY_BUCKET_NAME/$DEPLOY_APPSPEC_FILE,bundleType=YAML}"
 
 aws deploy create-deployment --application-name=$DEPLOY_APPLICATION_NAME --deployment-group-name=$DEPLOYMENT_GROUP_NAME --revision=$REVISION --deployment-config-name='CodeDeployDefault.LambdaCanary10Percent1Minutes'
 
